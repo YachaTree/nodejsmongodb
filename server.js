374c75ec9991dd7b3e216cd727f428fe49c27146
YachaTree <dnftksghkfkd@icloud.com>
2024-07-22 14:32:24 +0900

    삭제 기능 구현

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express"); // express 라이브러리 사용하겠다는 뜻
 const app = express();
+const { MongoClient, ObjectId } = require("mongodb"); //mongodb 서버 연결
+const url = require("./key.js"); // key.js에서 url 변수 요청해서 가져오기
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
 //css파일 적용, __dirname = "directory와 name의 합성어" 현재의 파일(file)이 위치한 폴더(directory)의 절대경로(absolute path)를 알려줌
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs"); //ejs 템플릿 적용
 app.use(express.json()); //
 app.use(express.urlencoded({ extended: true }));
-
-const { MongoClient, ObjectId } = require("mongodb"); //mongodb 서버 연결
-const url = require("./key.js"); // key.js에서 url 변수 요청해서 가져오기
 
 let db;
 new MongoClient(url)
@@ -103,9 +104,8 @@ app.get("/edit/:id", async (req, res) => {
   }
 });
 
-
 /*************** 게시물 수정 ****************/
-app.post("/edit", async (req, res) => {
+app.put("/edit", async (req, res) => {
   console.log(req.body);
 
   try {
@@ -113,14 +113,52 @@ app.post("/edit", async (req, res) => {
       res.send("제목 입력 안했는데?");
     } else {
       await db.collection("post").updateOne(
-        {_id: new ObjectId(req.body._id)},
-        {$set : {title: req.body.title,
-        content: req.body.content}
-      });
+        //db 추가하는 메서드
+        { _id: new ObjectId(req.body._id) },
+        { $set: { title: req.body.title, content: req.body.content } }
+      );
       res.redirect("/list");
     }
   } catch (error) {
     console.log(error);
     res.status(500).send("서버에러남");
   }
+});
+
+/*************** 게시물 삭제 ****************/
+app.delete("/delete", async (req, res) => {
+  console.log(req.query);
+
+  await db.collection("post").deleteOne(
+    //db 삭제하는 메서드
+    { _id: new ObjectId(req.query.docid) }
+  );
+  res.send("삭제완료");
+});
+
+// document.querySelectorAll(".btn")[0].addEventListener("click", function () {});
+
+/*************** 글 리스트 페이지별로 요청****************/
+app.get("/list/:id", async (req, res) => {
+  let result = await db
+    .collection("post")
+    .find()
+    .skip((req.params.id - 1) * 5)
+    .limit(5)
+    .toArray(); // post라는 컬렉션 폴더에 5개 도큐먼트(toArray) 가져온다
+  res.render("list.ejs", { 글목록: result });
+  //1번~5번까지 글 찾아서 result변수에 저장
+});
+
+/*************** 글 리스트 페이지별로 요청****************/
+app.get("/list/next/:id", async (req, res) => {
+  let result = await db
+    .collection("post")
+    .find({
+      _id: { $gt: new ObjectId(req.params.id) },
+    })
+    .limit(5)
+    .toArray(); // post라는 컬렉션 폴더에 5개 도큐먼트(toArray) 가져온다
+  res.render("list.ejs", { 글목록: result });
+  //1번~5번까지 글 찾아서 result변수에 저장
 });
